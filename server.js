7fd5fb79d0865cb934e7d145f8604d9a41040692
SakshamBadole <sakshambadole19@gmail.com>
2024-10-30 23:31:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3028;
+
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`); 
+});
